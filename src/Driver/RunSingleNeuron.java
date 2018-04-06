c927a43b44cdf5d8ccdf2bd857c67da7b6bd2505
Leslie S. Smith <l.s.smith@cs.stir.ac.uk>
2018-04-06 09:30:52 +0100

    updated to solve fix

--- src/Driver/RunSingleNeuron.java
+++ src/Driver/RunSingleNeuron.java
@@ -33,13 +33,17 @@ public class RunSingleNeuron {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		List<String> spikeList ;
+		double [][] contextArray ;
+		double [][] drivingArray ;
 		int argno = 0 ;
 		while (argno < args.length)
 			switch(args[argno]){
-			case "-f": // followed by input spike file name, so get file name for external spike inputs
-				Path inputFilePath = Paths.get(args[argno + 1]); // get the path to the input file 
-				spikeList = Files.readAllLines(inputFilePath);
+			case "-c": // followed by input spike file name, so get file name for external contextual spike inputs
+				contextArray = readInputsToArrayFromFile(args[argno + 1]);
+				argno = argno + 2 ;
+				break;
+			case "-d": // followed by input spike file name, so get file name for external driving spike inputs
+				drivingArray = readInputsToArrayFromFile(args[argno + 1]);	
 				argno = argno + 2 ;
 				break;
 			default:
@@ -51,6 +55,39 @@ public class RunSingleNeuron {
 		// set up the neuron,
 		PyramidalNeuron neuron = new PyramidalNeuron("SingleNeuron") ;
 
+	}
+	
+	private static double[][] readInputsToArrayFromFile(String filename )  throws IOException
+	/*
+	 * Reads the input in to a 2D
+	 * array where the array is N by 2, with each row being input
+	 * (neuron/channel) number, time (seconds)
+	 */
+	{
+		int lineNo = 0;
+		List<String> InputList = null;
+
+		Path inputFilePath = Paths.get(filename);
+		try {
+		InputList = Files.readAllLines(inputFilePath);
+		} catch (IOException e)
+		{
+			System.err.println("readInputsToArrayFromFile: Caught IOException: " + e.getMessage());
+			System.exit(1); 
+		}
+
+		double[][] inputArray = new double[InputList.size()][2];
+		// now turn InputList into an array, line by line
+		Iterator<String> inputIterator = InputList.iterator();
+		while (inputIterator.hasNext()) {
+			String nextInput = inputIterator.next().trim();
+			String[] inputComponents = nextInput.split("\\s+", 2);
+			for (int i = 0; i < 2; i++) {
+				inputArray[lineNo][i] = Double.parseDouble(inputComponents[i]);
+			}
+			lineNo = lineNo + 1;
+		}
+		return inputArray;
 	}
 
 }
